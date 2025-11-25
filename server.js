d060235782b005295c9b88ff3016ff543a33de8b
marie <borisova.marie1@gmail.com>
2025-11-25 19:30:57 +0100

    Added logic for fetching DNS lookup

--- server.js
+++ server.js
@@ -1,2 +1,24 @@
 import express from 'express';
 import fetch from 'node-fetch';
+
+const app = express();
+
+async function dnsLookup(urlString) {
+    try {
+        const url = new URL(urlString);
+        const hostname = url.hostname;
+
+        const apiUrl = `https://cloudflare-dns.com/dns-query?name=${hostname}&type=A`;
+
+        const res = await fetch(apiUrl, {
+            headers: { 'Accept': 'application/dns-json' }
+        });
+
+        const data = await res.json();
+
+        console.log(data);
+    }catch (err){
+        return { status: 'error', message: err.message };
+    }
+
+}
